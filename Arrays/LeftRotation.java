a80fb5a24e411ef04fb1e343870b86c8ab1cfc35
MouadAe <mouadaouane49@gmail.com>
2021-09-18 22:31:47 +0100

    feat: LeftRotation challenge solved using java

---
+++ Arrays/LeftRotation.java
@@ -0,0 +1,69 @@
+package Arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+    // Write your code here
+        int arrSize = a.size();
+        int[] arrCopy = new int[arrSize];
+        for (int i=0; i<arrSize; i++){
+            if(i-d<0) arrCopy[i-d+arrSize] = a.get(i);
+            else arrCopy[i-d] = a.get(i);
+        }
+        List<Integer> intList = new ArrayList<Integer>(arrCopy.length);
+        for (int i : arrCopy){intList.add(i);}
+        return intList;
+    }
+}
+
+public class LeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.rotLeft(a, d);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
